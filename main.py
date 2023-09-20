d89b19da423ab9c0f2bdae67a79c703b0991e5ae
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-09-20 13:27:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import discord
 import yt_dlp
 import boto3
-import pyyaml
+import yaml
 import os
+import argparse
 
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', default='config.yml', help='Path to the configuration file')
+args = parser.parse_args()
+
+# Load configurations from the YAML file
+with open(args.config, "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+# Initialize boto3 client for S3
+s3 = boto3.client('s3',endpoint_url=cfg['s3-endpoint'],aws_access_key_id=cfg['s3-access-key'],aws_secret_access_key=cfg['s3-secret-key'])
+
+# Discord Bot parts
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
@@ -21,21 +35,26 @@ async def on_message(message):
     if 'x.com' in message.content:
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'outtmpl': '/root/discord/bot/downloads/%(uploader_id)s-%(id)s.%(ext)s',
-            'cookiefile': '/root/discord/bot/cookies-twitter.txt'
+            'outtmpl': cfg['cache-directory'] + '/%(uploader_id)s-%(id)s.%(ext)s',
+            'cookiefile': cfg['x-cookie']
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(message.content, download=True)
             filename = ydl.prepare_filename(info_dict)
 
         s3_filename = os.path.basename(filename)
-        os.system(f's3cmd put {filename} s3://Your-Bucket-Name/{s3_filename} --acl-public')
+        if cfg['enable-s3-backup']:
+            # Upload file to S3
+            with open(filename, 'rb') as data:
+                s3.upload_fileobj(data, cfg['s3-bucket-name'], s3_filename, ExtraArgs={'ACL': 'public-read'})
 
-        reply_message = f"""Original Address:\n```{message.content}```\nObject Storage Address:\n```https://Your-S3-Address/Your-Bucket-Name/{s3_filename}```"""
-        await message.channel.send(reply_message, file=discord.File(f'/root/discord/Quincy0v0/downloads/{s3_filename}'))
+            reply_message = f"""Original Address:\n```{message.content}```\nObject Storage Address:\n```{cfg["s3-endpoint"]}/{cfg["s3-bucket-name"]}/{s3_filename}```"""
+            await message.channel.send(reply_message, file=discord.File(cfg['cache-directory'] + '/' + s3_filename))
+        else:
+            reply_message = f"""Original Address:\n```{message.content}```"""
+            await message.channel.send(reply_message, file=discord.File(cfg['cache-directory'] + '/' + s3_filename))
 
-        os.remove(filename)
+        if cfg['cache-clean']:
+            os.remove(filename)
 
-
-
-client.run('Your-Discord-App-Token')
+client.run(cfg['discord-bot-token'])
